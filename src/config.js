81ffe8d1e97fab7918b7e743e52303860bec5aa3
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2018-06-07 18:13:52 +0300

    Added docs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     dev: process.env.NODE_ENV !== 'production',
     production: process.env.NODE_ENV === 'production',
-    dialogFlowAccessToken: '5ede9a14ce884c3894f9c52a1cf190111'
+    dialogFlowAccessToken: process.env.DIALOGFLOW_TOKEN || '5ede9a14ce884c3894f9c52a1cf19011'
 }
 
 export default config;
